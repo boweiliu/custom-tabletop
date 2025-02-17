f4b7b41cddd0f997ced5c04adb437025dd5d0df5
Bowei Liu <liubowei@gmail.com>
2025-02-16 23:39:50 -0800

    noop

---
+++ src/lib/CardStoreProvider.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { useConvexClient } from 'convex-svelte';
+  import { getCardStore } from './cardStore';
+  import type { Snippet } from 'svelte';
+
+  const { children } = $props<{
+    children: Snippet;
+  }>();
+
+  const convexClient = useConvexClient();
+  const cardStore = getCardStore(convexClient);
+
+  // Make cardStore globally available
+  // $state.global({ cardStore });
+</script>
+
+{@render children()}
